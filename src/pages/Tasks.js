27528eee575aa129dcb6368161b035a503b9b1bf
RubenSilva13 <rubenmbs123@gmail.com>
2026-04-25 15:32:26 +0100

    Pagina tasks

---
+++ src/pages/Tasks.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
+import api from '../services/api';
+
+function Tasks () {
+    const [tasks, setTasks] = useState([]);
+    const [tittle, setTittle] = useState('');
+    const navigate = useNavigate();
+
+    const fetchTasks = async () => {
+        try {
+            const response = await api.get('/tasks');
+            setTasks(response.data);
+        } catch (err) {
+            navigate('/');
+        }
+    };
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
+    const handleCreate = async (e) => {
+        e.preventDefault();
+        try {
+            await api.post('/tasks', {tittle});
+            setTittle('');
+            fetchTasks();
+
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            await api.delete(`/tasks/${id}`);
+            fetchTasks();
+        } catch (err) {
+            console.error(err);
+        }
+    }; 
+
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate('/');
+    };
+
+
+    return (
+        <div>
+            <h2> As minhas tarefas</h2>
+            <button onClick ={handleLogout}>Logout</button>
+            <form onSubmit={handleCreate}>
+                <input
+                    type="text"
+                    placeholder="Nova tarefa"
+                    value= {tittle}
+                    onChange={(e) => setTittle(e.target.value)} 
+                />
+                <button type ="submit"> Adicionar</button>
+            </form>
+            <ul>
+                {tasks.map((task)  => (
+                <li key={task.id}>
+                    {task.titlle}
+                    <button onClick={() => handleDelete(task.id)}>Remover</button>
+                </li>
+                ))}
+            </ul>
+        </div>
+
+    );
+}
